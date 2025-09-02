159a873e60dbc173376707f5ef9a5e927c854b58
Sintja <sporeci2000@gmail.com>
2025-09-02 09:08:15 -0400

    Server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+// Load environment variables
 require('dotenv').config();
+
+// DB connection
+require('./config/db-connection')
+
 const express = require('express');
 
-//Import passport to use as middleware
-const passport = require ('passport');
+// Invoke that imported express method to create a new Express application, and I'm storing it inside of this variable app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Get our server running
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
